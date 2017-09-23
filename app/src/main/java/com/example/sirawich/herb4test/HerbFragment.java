324e0059b70a3d48767160fa6383557bc192dc15
Ex <sirawit0676@gmail.com>
2017-09-24 04:43:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sirawich/herb4test/HerbFragment.java
@@ -0,0 +1,26 @@
+package com.example.sirawich.herb4test;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HerbFragment extends Fragment {
+
+    public HerbFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_herb_fragment, container, false);
+    }
+
+}
